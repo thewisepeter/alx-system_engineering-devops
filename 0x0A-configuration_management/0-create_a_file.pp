d4769b6d1ba48d60aacc05088c366600f33b2851
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-25 13:53:41 +0300

    styling sorted

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file with specified contents
 file {'school':
-    path => '/tmp/school',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
