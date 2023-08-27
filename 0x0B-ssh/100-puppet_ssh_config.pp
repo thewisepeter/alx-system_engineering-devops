6f396c9e4e0414c0ea2253f6a46a89deaa10490a
thewisepeter <rempeterwisdom@gmail.com>
2023-08-26 22:31:33 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+#set up your client SSH configuration file
+#so that you can connect to a server
+#without typing a password.
+
+file {'/etc/ssh/ssh_config':
+	ensure => present,
+Host*
+	PasswordAuthentication no
+	IdentityFile ~/.ssh/school
