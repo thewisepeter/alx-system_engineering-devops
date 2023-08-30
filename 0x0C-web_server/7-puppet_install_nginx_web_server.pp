0e471bb84b3de28afd37551b57d323abb56e5039
Peter Ochan <rempeterwisdom@gmail.com>
2023-08-31 01:08:31 +0300

    puppet nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,50 +1,36 @@
 # Install Nginx web server (w/ Puppet)
 
-# Define a class for the Nginx configuration
-class nginx_config {
-    package { 'nginx':
-        ensure => 'installed',
-    }
+# declare a config variable with set conditions
+$config = "server {
+	listen 80 default_server;
+        listen [::]:80 default_server;
 
-    file { '/var/www/html/index.html':
-        content => 'Hello World!',
-        require => Package['nginx'],
-    }
+        root /var/www/html;
+        index index.html;
 
-    service { 'nginx':
-        ensure => 'running',
-        enable => true,
-        require => Package['nginx'],
-    }
-}
-
-# Define a class for the redirection configuration
-class redirect_config {
-    file { '/etc/nginx/sites-available/redirect':
-        ensure => 'present',
-        content => "server {
-            listen 80;
-            server_name _;
-            location /redirect_me {
+        location /redirect_me {
                 return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-            }
-        }",
-        require => Class['nginx_config'],
-    }
+        }
+}"
 
-    file { '/etc/nginx/sites-enabled/redirect':
-        ensure => 'link',
-        target => '/etc/nginx/sites-available/redirect',
-        require => File['/etc/nginx/sites-available/redirect'],
-    }
-
-    exec { 'nginx_reload':
-        command => 'service nginx reload',
-        refreshonly => true,
-        subscribe => [File['/etc/nginx/sites-available/redirect'], File['/etc/nginx/sites-enabled/redirect']],
-    }
+# Installs an Nginx server
+package { 'nginx':
+ensure	=> 'installed',
 }
 
-# Apply both classes
-include nginx_config
-include redirect_config
+file { 'index.html':
+ensure	=> 'present',
+path	=> '/var/www/html/index.html',
+content	=> 'Hello World!',
+mode	=> '0644'
+}
+
+file { 'server_config':
+ensure	=> 'present',
+path 	=> '/etc/nginx/sites-available/default',
+content => $config
+}
+
+exec { 'service nginx restart':
+path	=> ['/usr/sbin', '/usr/bin']
+}
