555cd7b487bb3c86a615b749dac3e39a4cb9451d
Peter Ochan <rempeterwisdom@gmail.com>
2023-10-10 13:32:24 +0300

    try

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-# script that finds users on subreddit
+"""
+number of subscribers for a given subreddit
+"""
+
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries Reddit Api for number of subscribers
-    for a given subreddit
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    user_agent = {'User-Agent': 'Google Chrome Version 117.0.5938.149'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
-    data = response.json()
+    results = response.json()
 
     try:
-        return data.get('data').get('subscribers')
+        return results.get('data').get('subscribers')
+
     except Exception:
         return 0
