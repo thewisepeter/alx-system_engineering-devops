98c3077d3ec1210b6d5fa22ea65d61d5cdec877f
Peter Ochan <rempeterwisdom@gmail.com>
2023-10-10 13:11:43 +0300

    changed format

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     user_agent = {'User-Agent': 'Google Chrome Version 117.0.5938.149'}
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = get(url, headers=user_agent)
     data = response.json()
 
