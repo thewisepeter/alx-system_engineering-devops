5b78b69cb90867f80633f7a47bb8bdea15bd83ed
Peter Ochan <rempeterwisdom@gmail.com>
2023-10-10 13:40:26 +0300

    fixed commenting

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# script that finds users on subreddit
+"""
+    script that finds users on subreddit
+"""
 
 from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
+    function that queries Reddit Api for number of subscribers
+    for a given subreddit
     """
 
     if subreddit is None or not isinstance(subreddit, str):
